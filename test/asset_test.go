3483d202c3db18757eea96262b0dcbab93acf42e
Dan Ekström <dan@oakenstream.com>
2020-12-29 18:24:13 +0100

    Added test case for asset

--- test/asset_test.go
+++ test/asset_test.go
@@ -1,3 +1,25 @@
 // +build test
 
-package generationk
+package generationk_test
+
+import (
+	K "generationk/Asset"
+	"testing"
+)
+
+func TestCreateAsset(t *testing.T) {
+	asset := K.NewAsset("test", K.OHLC{1.0, 2.0, 3.0, 4.0, 5})
+
+	if asset != nil && isTest(asset) {
+		t.Fatalf(`Asset not of type Asset`)
+	}
+}
+
+func isTest(t interface{}) bool {
+	switch t.(type) {
+	case K.Asset:
+		return true
+	default:
+		return false
+	}
+}
